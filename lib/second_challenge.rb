d876ae9f221ca7dccf4c6bc875f6d9c797a59977
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 10:00:27 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,23 @@
 def second_challenge
   epic_tragedy = {
-    # :montague => {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {  },
+      :heroine => {},
+      :heroine_friends => []
+   }
+  }
+
+  
+end
+  # epic_tragedy = {
+  #   # :montague => {
     #   :patriarch=>{},
     #   :matriarch=>{},
     #   :hero=>{},
