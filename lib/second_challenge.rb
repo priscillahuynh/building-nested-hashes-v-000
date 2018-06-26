49ea4578afc3e4808d3df4ad7a7d54cce7903f87
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 10:01:14 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -14,18 +14,3 @@ def second_challenge
    }
   }
 end
-  # epic_tragedy = {
-  #   # :montague => {
-    #   :patriarch=>{},
-    #   :matriarch=>{},
-    #   :hero=>{},
-    #   :hero_friends=>[]
-      # },
-    # :capulet=> {
-    #   :patriarch=>{},
-    #   :matriarch=>{},
-    #   :heroine=>{},
-    #   :heroine_friends=>[]
-    #   }
-#     }
-# end
